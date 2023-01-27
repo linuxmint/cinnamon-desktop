d880133b38da46b22421f38639ddd384692002f0
Michael Webster <miketwebster@gmail.com>
2023-01-26 20:19:03 -0500

    Add new enums, settings key for xdg-desktop-portal color-scheme.

--- libcinnamon-desktop/cdesktop-enums.h
+++ libcinnamon-desktop/cdesktop-enums.h
@@ -204,6 +204,13 @@ typedef enum
 
 typedef enum
 {
+  C_DESKTOP_COLOR_SCHEME_DEFAULT,
+  C_DESKTOP_COLOR_SCHEME_PREFER_DARK,
+  C_DESKTOP_COLOR_SCHEME_PREFER_LIGHT,
+} CDesktopColorScheme;
+
+typedef enum
+{
 /* All bindings before _SEPARATOR are treated as
  * "global" bindings, i.e. they work regardless of
  * Cinnamon's global state (open menus, etc...)
